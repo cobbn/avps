2390ea140777ce19b4587f10130eef2cfa05e296
cobbn <168705970+cobbn@users.noreply.github.com>
2024-06-19 16:24:28 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -199,7 +199,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b><a href ="https://t.me/Reaperzclub">Powered By Reaper's CLub</a></b>'
+    msg = '<b><a href ="https://t.me/Reaperzclub">Powered By Reapers-Club</a></b>'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
